f92afd1e7f8e950c2eee5cfe3a8953758f00a212
cuiqinxin <cqx2410@163.com>
2018-08-08 11:05:00 +0800

    头部搜索

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,30 @@
     <div class="top hidden-md-and-down">
       <el-row class="top-con" type="flex" justify="space-between">
         <el-col :span="12" class="top-left">
-          <!-- <button @click="link">等</button> -->
-          <router-link to="" 
-            
-            v-if="!sta">
-            {{userPhoneNumber}}{{userPhoneNumber1}}
-          </router-link>
+          <div class="topHover">
+            <router-link to="/member/memberorder" 
+              v-if="!sta">
+              {{userPhoneNumber}}{{userPhoneNumber1}}
+            </router-link>
+            <div class="member">
+              <div class="member-img"><img src="../static/pep.png"></div>
+              <div>
+                <p class="guanli">
+                  <router-link to="/member/memberorder">账号管理</router-link>
+                  <span></span>
+                  <a @click="logout">退出</a>
+                </p>
+                <p>{{userPhoneNumber}}{{userPhoneNumber1}}</p>
+                <p class="join"><router-link to="/join">加盟我们</router-link></p>
+              </div>
+            </div>
+          </div>
           <span>欢迎来到信达！</span>
           <router-link :to="{path:'/outter/login',query:{id:123}}" v-if="sta">登录</router-link>
           <router-link to="/outter/zhuce" v-if="sta">快速注册</router-link>
         </el-col>
         <el-col :span="12" class="top-right">
-          <router-link to="" class="shop-cart">
+          <router-link to="" class="shop-cart" v-if="!sta">
             <span class="shop-img"></span>
             <router-link to="/shoppingcart">购物车<a>{{cartNum}}</a>件</router-link>
           </router-link>
@@ -45,13 +57,40 @@ export default {
         userPhoneNumber1:'',
       };
     },
-    // methods:{
-    //     link(){
-    //         this.$router.push({
-    //             path:'/outter/login',query:{id:123456}
-    //         })
-    //     },
-    // },
+    methods:{
+        // link(){
+        //     this.$router.push({
+        //         path:'/outter/login',query:{id:123456}
+        //     })
+        // },
+      // logout(){
+      //   this.ajax.post("/xinda-api/sso/ logout").then(data=>{
+      //     this.cartNum = data.data.data.cartNum;
+      //   });
+      // }
+      logout() {
+        this.$confirm('是否退出登录?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          // store.commit('loginStatus',that.phoneValue)
+          this.ajax.post("/xinda-api/sso/logout").then(data=>{
+            // console.log(data.data)
+            // this.cartNum = data.data.data.cartNum;
+          });
+          this.$message({
+            type: 'success',
+            message: '成功退出登陆!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消退出'
+          });          
+        });
+      }
+    },
     computed:{
       userPhoneNumber(){
         return store.state.userPhoneNumber;
@@ -119,10 +158,22 @@ export default {
       height: 35px;
       line-height: 35px;
       .top-left{
-        a{
-          color:#2693d4;
-          margin: 0 12px;
+        >a{
+          color: #2693d4;
         }
+        >div{
+          display: inline-block;
+          a{
+            position: relative;
+            color:#2693d4;
+            margin: 0 12px;
+          }
+        }
+        // a{
+        //   position: relative;
+        //   color:#2693d4;
+        //   margin: 0 12px;
+        // }
       }
       .top-right{
         text-align: right;
@@ -168,6 +219,68 @@ export default {
     }
     .color_red input{
       border: 2px solid red!important;
+    }
+  }
+  .member{
+    position: absolute;
+    background-color: #fff;
+    border: 1px solid rgb(230, 230, 230);
+    border-top: none;
+    z-index: 9999;
+    // display: flex;
+    display: none;
+    padding: 0 10px 10px;
+    .member-img{
+      width: 50px;
+      height: 50px;
+      margin:20px 10px;
+      img{
+        width: 100%;
+      }
+    }
+    .guanli{
+      line-height: 26px!important;
+      width: 135px;
+      text-align: right;
+      a{
+        cursor: pointer;
+        display: inline-block;
+        color: #000!important;
+        margin: 0!important;
+        line-height: 26px!important;
+        &:hover{
+          color: #2693d4!important;
+        }
+      }
+      span{
+        display: inline-block;
+        width: 1px;
+        height: 14px;
+        background-color: #999;
+        vertical-align: middle;
+      }
+    }
+    .join{
+      border: 1px solid #FFE8DE;
+      background: #FFF0E8;
+      text-align: center;
+      // margin: 10px;
+      line-height: 26px;
+      &:hover a{
+        color: #f40!important;
+      }
+      a{
+        color: #000!important;
+      }
+    }
+  }
+  .topHover{
+    &:hover {
+      background-color: #fff;
+      // border: 1px solid #999;
+      .member{
+        display: flex;
+      }
     }
   }
 
