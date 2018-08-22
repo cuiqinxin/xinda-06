c10481022a684de85b1da7308e9295aa57c9bd4c
shijipeng <1054340827@qq.com>
2018-08-22 16:12:04 +0800

    路由跳转黑名单

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,10 @@
 
 // Vue.use(vuex)
 
+
+
+
+
 export default new Vuex.Store({
     //设置全局变量  所有组件都能用
     state:{
@@ -56,11 +60,6 @@ export default new Vuex.Store({
     },
 
 
-    // //有关全局变量的全局函数
-    // getters:{
-    //     showLoading(state){
-    //         return state.showLoading 
-    //     },
-    // },
+
    
 });
